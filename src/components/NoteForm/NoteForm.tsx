420fe1c72c9db9a67a5b4327bac2c857765236b2
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-06-10 10:23:44 -0600

    add noteForm

---
+++ src/components/NoteForm/NoteForm.tsx
@@ -0,0 +1,54 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import css from "./NoteForm.module.css";
+
+interface NoteFormProps {
+  onSubmit: (values: {
+    title: string;
+    content: string;
+    tag: string;
+  }) => void;
+}
+
+const schema = Yup.object({
+  title: Yup.string()
+    .min(3)
+    .max(50)
+    .required(),
+
+  content: Yup.string().max(500),
+
+  tag: Yup.string()
+    .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"])
+    .required(),
+});
+
+export default function NoteForm({ onSubmit }: NoteFormProps) {
+  return (
+    <Formik
+      initialValues={{ title: "", content: "", tag: "Todo" }}
+      validationSchema={schema}
+      onSubmit={onSubmit}
+    >
+      {() => (
+        <Form className={css.form}>
+          <Field name="title" />
+          <ErrorMessage name="title" component="span" />
+
+          <Field as="textarea" name="content" />
+          <ErrorMessage name="content" component="span" />
+
+          <Field as="select" name="tag">
+            <option value="Todo">Todo</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            <option value="Meeting">Meeting</option>
+            <option value="Shopping">Shopping</option>
+          </Field>
+
+          <button type="submit">Create note</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
